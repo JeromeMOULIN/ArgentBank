90fc5aa8f42d2d41b81a3fa8d5669ea4d7344ebe
JeromeMOULIN <j.moulin89@laposte.net>
2023-12-14 15:54:21 +0100

    Change structure for Back and Front folder, add react+vite in Front folder. Adding router in react

---
+++ front/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components')
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages')
+      }
+    ]
+  }
+})
