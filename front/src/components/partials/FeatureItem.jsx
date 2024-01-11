721910fe6f6270ad7f5af9e284caace0e2271b55
JeromeMOULIN <j.moulin89@laposte.net>
2024-01-11 15:58:25 +0100

    change Feature in home page into a component

---
+++ front/src/components/partials/FeatureItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const FeatureItem = (props) => {
+
+    console.log(props)
+    return (
+        
+        <div className="feature-item">
+            <img
+                src={props.src}
+                alt={props.alt}
+                className="feature-icon"
+            />
+            <h3 className="feature-item-title">{props.title}</h3>
+            <p>{props.text}</p>
+        </div>
+    );
+};
+
+export default FeatureItem;
