0259f97e99c92b763499632fa7925c05689ed0da
JeromeMOULIN <j.moulin89@laposte.net>
2024-01-12 15:40:48 +0100

    adding reduced img for icon in home page

--- front/src/pages/public/Home.jsx
+++ front/src/pages/public/Home.jsx
@@ -18,18 +18,18 @@ const Home = () => {
             <section className="features">
                 <h2 className="sr-only">Features</h2>
 
-                <FeatureItem src="src/assets/img/icon-chat.png" alt="Chat Icon"
+                <FeatureItem src="src/assets/img/icon-chat100.png" alt="Chat Icon"
                     title="More savings means higher rates"
                     text="Need to talk to a representative? You can get in touch through our
                         24/7 chat or through a phone call in less than 5 minutes."
                 />
 
-                <FeatureItem src="src/assets/img/icon-money.png" alt="Money Icon"
+                <FeatureItem src="src/assets/img/icon-money100.png" alt="Money Icon"
                     title="You are our #1 priority"
                     text="The more you save with us, the higher your interest rate will be!"
                 />
 
-                <FeatureItem src="src/assets/img/icon-security.png" alt="Shield Icon"
+                <FeatureItem src="src/assets/img/icon-security100.png" alt="Shield Icon"
                     title="Security you can trust"
                     text="We use top of the line encryption to make sure your data and money
                 is always safe."
